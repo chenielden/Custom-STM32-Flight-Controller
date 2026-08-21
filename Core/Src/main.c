610b41b168a61d1e744197627f03f34442dac16f
Daniel Chen <chend127@mcmaster.ca>
2026-08-21 18:11:56 -0400

    Initial STM32F405 flight controller setup

---
+++ Core/Src/main.c
@@ -0,0 +1,188 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+#include "main.h"
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi1;
+
+/* USER CODE BEGIN PV */
+volatile uint8_t imu_who_am_i = 0;
+volatile HAL_StatusTypeDef imu_spi_status;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI1_Init(void);
+
+/* USER CODE BEGIN PFP */
+uint8_t IMU_ReadRegister(uint8_t reg);
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+uint8_t IMU_ReadRegister(uint8_t reg)
+{
+    uint8_t tx[2];
+    uint8_t rx[2];
+
+    /* Bit 7 = 1 means READ for MPU-family SPI */
+    tx[0] = reg | 0x80;
+    tx[1] = 0x00;
+
+    /* Select IMU */
+    HAL_GPIO_WritePin(IMU_CS_GPIO_Port,
+                      IMU_CS_Pin,
+                      GPIO_PIN_RESET);
+
+    /* Send register address and receive response */
+    imu_spi_status = HAL_SPI_TransmitReceive(&hspi1,
+                                             tx,
+                                             rx,
+                                             2,
+                                             100);
+
+    /* Deselect IMU */
+    HAL_GPIO_WritePin(IMU_CS_GPIO_Port,
+                      IMU_CS_Pin,
+                      GPIO_PIN_SET);
+
+    return rx[1];
+}
+
+/* USER CODE END 0 */
+
+int main(void)
+{
+    HAL_Init();
+
+    SystemClock_Config();
+
+    MX_GPIO_Init();
+    MX_SPI1_Init();
+
+    /* Give the IMU time to power up */
+    HAL_Delay(100);
+
+    /* MPU WHO_AM_I register = 0x75 */
+    imu_who_am_i = IMU_ReadRegister(0x75);
+
+    while (1)
+    {
+        /*
+         * Nothing needed here yet.
+         * imu_who_am_i stores the sensor ID.
+         */
+        HAL_Delay(1000);
+    }
+}
+
+/**
+  * @brief System Clock Configuration
+  */
+void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    RCC_ClkInitStruct.ClockType =
+        RCC_CLOCKTYPE_HCLK |
+        RCC_CLOCKTYPE_SYSCLK |
+        RCC_CLOCKTYPE_PCLK1 |
+        RCC_CLOCKTYPE_PCLK2;
+
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct,
+                            FLASH_LATENCY_0) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+/**
+  * @brief SPI1 Initialization Function
+  */
+static void MX_SPI1_Init(void)
+{
+    hspi1.Instance = SPI1;
+    hspi1.Init.Mode = SPI_MODE_MASTER;
+    hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+    hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+    hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+    hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+    hspi1.Init.NSS = SPI_NSS_SOFT;
+    hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
+    hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+    hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+    hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+    hspi1.Init.CRCPolynomial = 10;
+
+    if (HAL_SPI_Init(&hspi1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  */
+static void MX_GPIO_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    /* CS starts HIGH = IMU not selected */
+    HAL_GPIO_WritePin(IMU_CS_GPIO_Port,
+                      IMU_CS_Pin,
+                      GPIO_PIN_SET);
+
+    GPIO_InitStruct.Pin = IMU_CS_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+    HAL_GPIO_Init(IMU_CS_GPIO_Port,
+                  &GPIO_InitStruct);
+}
+
+void Error_Handler(void)
+{
+    __disable_irq();
+
+    while (1)
+    {
+    }
+}
+
+#ifdef USE_FULL_ASSERT
+
+void assert_failed(uint8_t *file, uint32_t line)
+{
+}
+
+#endif
